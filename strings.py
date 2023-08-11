2aaf2dd3d2caddb42a2e8491efc39bc82edcce44
GUILLERMO IAN BUMATAY <guillermoianbumatay@GUILLERMOs-Air.fios-router.home>
2023-08-10 23:57:39 -0400

    print string

---
+++ strings.py
@@ -0,0 +1,27 @@
+# this is a comment => command /
+
+print('Hello world!')   
+
+print('Hello world!\nHello world!\nHello world!')
+
+print('Hello' + ' ' + 'Ian!') # string concatenation 
+
+input_name = input('Type your name: ')
+print(input_name) 
+
+name_length = len(input_name) 
+print(name_length) 
+
+
+# Band generator 
+
+print('Welcome to band generator') 
+
+city = input('Which city you grew up in?: ') 
+print(city) 
+
+pet = input('what is the name of your first pet?: ')
+print(pet)  
+
+print('Your band name is' + ' ' + city + ' ' + pet)
+
