d39d06b71e94c3ac99fb92cab3b48f191a073c27
GUILLERMO IAN BUMATAY <guillermoianbumatay@GUILLERMOs-Air.fios-router.home>
2023-08-10 23:04:03 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# print("Hello")  
+
+
+
+
+# flow Control 
+# name = "Ian" 
+
+# if name == "boy": 
+#     print("Hello Ian!")
+# else: 
+#     print("intruder alert!") 
+
+
+# print("Enter name") 
+# name = input()
+
+# if name: 
+#     print("Thank you for entering your name")
+# else: 
+#     print("you did not enter name") 
+
+    # while loop 
+
+i = 0 
+
+while i < 5: 
+    print("Count to " + str(i))
+    i = i + 1
+
+
+
+
